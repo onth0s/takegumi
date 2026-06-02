0299696a7a077d03cdd28abadfcd74e57f24d287
onth0s <metaagnosis@gmail.com>
2026-06-02 19:48:15 +0200

    CHECKPOINT

--- src/components/layout/Editor/FloatingHeader.tsx
+++ src/components/layout/Editor/FloatingHeader.tsx
@@ -8,7 +8,7 @@ export default function FloatingHeader() {
   return (
     <div className="absolute top-6 left-4 z-10 select-none">
       <div
-        className="bg-black/70 flex w-10 h-10 justify-center items-center text-text-secondary rounded-sm border-accent border cursor-pointerz"
+        className="bg-black/70 flex w-10 h-10 justify-center items-center text-text-secondary rounded-sm border-accent border cursor-pointer"
         onClick={() => router.push("/")}
       >
         <p>&#8592;</p>
