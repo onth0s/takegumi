06563acdc8d25a0f53c9638cddd5589c51d29c42
onth0s <metaagnosis@gmail.com>
2026-06-02 17:02:25 +0200

    quicksave

--- src/components/layout/Home.jsx
+++ src/components/layout/Home.jsx
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen items-center justify-between  pt-24 pb-6 bg-amber-800_">
+    <div className="relative flex flex-col h-screen items-center justify-between  pt-24 pb-6 bg-amber-800_">
       <Header />
-
+      <Sidebar />
       <Center />
-
       <Footer />
     </div>
   );
@@ -22,14 +21,24 @@ const Header = () => {
   );
 };
 
+const Sidebar = () => {
+  return (
+    <div className="absolute left-8 top-16 w-1/4 h-4/5 border-2">
+      <p>recents will show up here</p>
+      <p>favorites, pinned</p>
+      <p>or simply a message that let&apos;s you know that will be the case</p>
+    </div>
+  );
+};
+
 const Center = () => {
   return (
-    <div className="flex flex-col justify-center_ w-full h-full bg-amber-950">
+    <div className="flex flex-col justify-center items-center w-full h-full bg-amber-950_ space-y-4 pl-32">
       <div className="flex items-center justify-center -mt-26 w-1/3 h-1/2 border-2">
-        <p>Click to add images or Drag & Drop</p>
+        <p>Click to add images or drag & drop them here</p>
       </div>
-    
-      <div>or import</div>
+
+      <div>or import [project JSON] or [Markdown script]</div>
     </div>
   );
 };
