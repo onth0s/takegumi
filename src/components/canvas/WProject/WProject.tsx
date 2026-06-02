e08fb75b5d3ee729a4ff4e0c37fd2f0f240db875
onth0s <metaagnosis@gmail.com>
2026-06-02 20:03:43 +0200

    quicksave

---
+++ src/components/canvas/WProject/WProject.tsx
@@ -0,0 +1,4 @@
+export default function WProject() {
+  return <div className="w-4/5 bg-white h-full" />;
+}
+z
--- src/components/layout/Editor/Viewport.tsx
+++ src/components/layout/Editor/Viewport.tsx
@@ -1,11 +1,9 @@
+import WProject from "../../canvas/WProject/WProject";
+
 export default function Viewport() {
   return (
     <div className="flex-1 bg-grid flex items-center justify-center">
       <WProject />
     </div>
   );
-}
-
-function WProject() {
-  return <div className="w-4/5 bg-white h-full" />;
 }
